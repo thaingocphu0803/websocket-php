8e9a7eec655916a8626f65fdeb1114ebb5ffc347
thaingocphu0803 <167735819+thaingocphu0803@users.noreply.github.com>
2024-12-31 09:07:29 +0700

    adding: edit fullname

--- public/page/my-profile.php
+++ public/page/my-profile.php
@@ -10,27 +10,25 @@
 
 			<input id="user_avt" name="user_avt" class="input-file" type="file" accept="image/*" onchange="handleUploadImages(event)">
 
-
-			<div class="fullname-group">
-				<span class="user-fullname">Thái Ngọc Phú</span>
-				<span  id="update_fullname_btn" title="Click to change your name">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20px" heigth="20px">
-						<path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
-					</svg>
-				</span>
-			</div>
-
 			<div class="edit-fullname-group">
 				<input
 					id="fullname_profile"
-					class="user-fullname"
 					name="fullname_profile"
 					type="text"
 					placeholder="Thái Ngọc Phú"
-					title="Must be at least 8 character and include one uppercase letter, one lowercase letter, one number, one special character.">
-				<div class="btn-group">
-					<button>ok</button>
-					<button>cancel</button>
+					value="Thái Ngọc Phú"
+					disabled>
+				<div class="fullname-btn-group">
+					<!-- open edit full name -->
+					<button id="update_fullname_btn"  class="edit-fullname-btn" title="Click to change your name">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16px" heigth="16px">
+							<path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+						</svg>
+					</button>
+					<!-- save edit full name -->
+					<button  class="edit-fullname-btn" id="save_fullname">Save</button>
+					<!-- cancel edit full name -->
+					<button class="edit-fullname-btn" id="cancel_fullname">Cancel</button>
 				</div>
 			</div>
 
@@ -45,8 +43,7 @@
 					class="input-form"
 					name="current_password"
 					type="password"
-					placeholder="Enter your current password"
-					title="Must be at least 8 character and include one uppercase letter, one lowercase letter, one number, one special character.">
+					placeholder="Enter your current password">
 			</div>
 
 			<div class="form_profile_input">
@@ -56,7 +53,8 @@
 					class="input-form"
 					name="new_password"
 					type="password"
-					placeholder="Enter your new password">
+					placeholder="Enter your new password"
+					title="Must be at least 8 character and include one uppercase letter, one lowercase letter, one number, one special character.">
 			</div>
 
 			<div class="form_profile_input">
@@ -66,7 +64,8 @@
 					class="input-form"
 					name="new_confirm_password"
 					type="password"
-					placeholder="Enter your new password again">
+					placeholder="Enter your new password again"
+					title="Must be at least 8 character and include one uppercase letter, one lowercase letter, one number, one special character.">
 			</div>
 
 			<div class="group-btn flex-bottom">
